54be4b800687f438006995d514c71eea35f19232
Kamal Deepak <kamaldsharma89@gmail.com>
2018-05-17 21:25:50 +0100

    Add timer and set break

--- take_a_break.py
+++ take_a_break.py
@@ -1,4 +1,11 @@
 import webbrowser
+import time
 
+num_of_breaks = 3
+break_count =0
 
-webbrowser.open("https://www.youtube.com/watch?v=99iyGGbaRWE")
+print("The timer starts at "+time.ctime())
+while(break_count<3):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=99iyGGbaRWE")
+    break_count = break_count + 1
